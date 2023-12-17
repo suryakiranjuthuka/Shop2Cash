e0f6ca19b0b005be64dc192c9d78c79663c07132
Surya Kiran Juthuka <suryakiran.kittu@gmail.com>
2023-12-17 14:41:48 +0530

    Porting to NPM + SASS + Parcel

--- src/index.php
+++ src/index.php
@@ -470,10 +470,10 @@
     </div>
 
 
-  <script src="js/vendor/modernizr-3.11.2.min.js"></script>
-  <script src="js/plugins.js"></script>
+  <script src="./js/modernizr-3.11.2.min.js"></script>
+  <script src="./js/plugins.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/umbrellajs"></script>
-  <script src="js/main.js"></script>
+  <script src="./js/main.js"></script>
 </body>
 
 </html>
