36d4b96380b24a388e8ce0c71a8970fa742f2a72
Surya Kiran Juthuka <suryakiran.kittu@gmail.com>
2023-12-26 20:59:58 +0530

    Individual Product Page Completed

--- src/js/main.js
+++ src/js/main.js
@@ -51,3 +51,44 @@ u('#productPage #info header').on('click', function(e) {
   u('#productPage #info').toggleClass("open");
   // u('#productsPage #filters').removeClass("popup");
 });
+
+
+
+
+
+
+
+// ######## Rewards Expiry CountDown Timer ########
+function countdown( elementName, minutes, seconds )
+{
+    var element, endTime, hours, mins, msLeft, time;
+    function twoDigits( n )
+
+    {return (n <= 9 ? "0" + n : n);}
+
+    element = document.getElementById( elementName );
+    endTime = (+new Date) + 1000 * (60*minutes + seconds) + 500;
+    updateTimer();
+
+
+    function updateTimer()
+    {
+        msLeft = endTime - (+new Date);
+
+        if ( msLeft < 1000 ) {
+            // element.innerHTML = "Time is up!";
+            element.children[0].innerHTML = "00";
+            element.children[1].innerHTML = "00";
+
+        } else {
+            time = new Date( msLeft );
+            hours = time.getUTCHours();
+            mins = time.getUTCMinutes();
+            element.children[0].innerHTML = (hours ? hours + ':' + twoDigits( mins ) : mins);
+            element.children[1].innerHTML = twoDigits( time.getUTCSeconds() )
+            // element.innerHTML = (hours ? hours + ':' + twoDigits( mins ) : mins) + ':' + twoDigits( time.getUTCSeconds() );
+            setTimeout( updateTimer, time.getUTCMilliseconds() + 500 );
+        }
+    }
+}
+countdown( "thirty-countdown", 30, 0 );
